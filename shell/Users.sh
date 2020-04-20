9469452c1672c27be4ce124aa5f76c585d10a51f
dengyingxu <408333056@qq.com>
2020-04-20 16:32:24 +0800

    完成用户检测脚本

--- shell/Users.sh
+++ shell/Users.sh
@@ -9,9 +9,10 @@ NowTime=`date +"%Y-%m-%d__%H:%M:%S"`
 
 UserNum=`cat /etc/passwd | grep home | grep bash | wc -l`
 Users=(`cat /etc/passwd | w | tail -n 3  | cut -d ' ' -f 1`)
-
-
-echo "${NowTime} ${LoadAvg} ${CpuUsedPerc} ${CpuTemp}℃  ${Warn}"
+UserRoot=(`cat /etc/group |grep wheel |cut -d ':' -f 4-|cut -d ',' -f 1-`)
+UserInfo=(`w | awk 'NR==1 {next} NR==2 {next} {printf ","$1"_"$3"_"$2}' \
+    | tr -d '\n'|cut -d ',' -f 2-`)
+echo "${NowTime} ${UserNum} [${Users[0]},${Users[1]},${Users[2]}] [${UserRoot[*]}] [${UserInfo}]"
 
 
 
